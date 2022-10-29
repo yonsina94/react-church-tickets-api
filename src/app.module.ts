be5c4aaf4d53ada3cc87d8e350374b181295dedf
Yansi Mora <yansimora@gmail.com>
2022-10-29 18:24:57 -0400

    changes applied

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TokenCardModule } from './components/token-card/token-card.module';
-import { TokensModule } from './components/token/token.module';
+import { TokenModule } from './components/token/token.module';
+import { HealthModule } from './components/health/health.module';
 import appConfig from './config/app.config';
 import databaseConfig from './config/database.config';
 import swaggerConfig from './config/swagger.config';
@@ -22,7 +23,8 @@ import swaggerConfig from './config/swagger.config';
       inject: [databaseConfig.KEY],
     }),
     TokenCardModule,
-    TokensModule,
+    TokenModule,
+    HealthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
+import { ConfigType } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import appConfig from './config/app.config';
+import swaggerConfig from './config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config: ConfigType<typeof appConfig> = app.get(appConfig.KEY);
+
+  const swaggerConf: ConfigType<typeof swaggerConfig> = app.get(
+    swaggerConfig.KEY,
+  );
+
+  //if (swaggerConf.enabled) {
+  const properties = new DocumentBuilder()
+    .setTitle(swaggerConf.title)
+    .setDescription(swaggerConf.description)
+    .build();
+  const document = SwaggerModule.createDocument(app, properties);
+  SwaggerModule.setup('swagger', app, document);
+  // }
+
+  await app.listen(config.port, () => {
+    console.log(`Server listen in http://${config.host}:${config.port}`);
+  });
 }
 bootstrap();
